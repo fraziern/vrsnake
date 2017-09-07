0dfde5d7e6c366581522b21b41477ed9dfc42813
Nick Frazier <nrflaw@gmail.com>
2017-09-07 08:44:26 -0400

    clean up initializer a bit

--- components/initializer.js
+++ components/initializer.js
@@ -5,16 +5,11 @@
 
 AFRAME.registerComponent("initializer", {
   init: function() {
-    let score = 0;
-    const bonusScore = 14;
+    this.score = 0;
     const scene = this.el;
     const head = document.querySelector("#headObj");
-    const scoreOutput = document.querySelector("#scoreOutput");
 
-    // Let scene settle before checking collisions
-    window.setTimeout(() => {
-      head.setAttribute("plane-collider", true);
-    }, 1000);
+    head.setAttribute("plane-collider", true);
 
     scene.setAttribute("snake-controller", { head });
     scene.setAttribute("wasd-relative", true); // depends on snake-controller
@@ -25,26 +20,30 @@ AFRAME.registerComponent("initializer", {
       scene.setAttribute("fog", "type: exponential; color: #AAA; density: .00");
     });
 
-    scene.addEventListener("gobbled-apple", () => {
-      if (scene.isPlaying) {
-        // remove apple
-        if (event.detail.el.classList.contains("apple")) {
-          event.detail.el.parentNode.removeChild(event.detail.el);
-        }
-        // update score
-        scoreOutput.innerText = ++score;
-        scoreOutput.classList.remove("score-animation");
-        void scoreOutput.offsetWidth;
-        scoreOutput.classList.add("score-animation");
-        // add apple if we're in bonus
-        if (score >= bonusScore) {
-          var entity = document.createElement("a-entity");
-          entity.setAttribute("mixin", "apple");
-          entity.setAttribute("class", "apple collidable");
-          entity.setAttribute("random-position", "fixedY: 1.25");
-          this.el.sceneEl.appendChild(entity);
-        }
+    scene.addEventListener("gobbled-apple", this.handleGobble.bind(this));
+  },
+
+  handleGobble(e) {
+    const bonusScore = 14;
+    const scoreOutput = document.querySelector("#scoreOutput");
+    if (this.el.isPlaying) {
+      // remove apple
+      if (e.detail.el.classList.contains("apple")) {
+        e.detail.el.parentNode.removeChild(e.detail.el);
       }
-    });
+      // update score - use css hack to trigger animation
+      scoreOutput.innerText = ++this.score;
+      scoreOutput.classList.remove("score-animation");
+      void scoreOutput.offsetWidth;
+      scoreOutput.classList.add("score-animation");
+      // add apple if we're in bonus
+      if (this.score >= bonusScore) {
+        var entity = document.createElement("a-entity");
+        entity.setAttribute("mixin", "apple");
+        entity.setAttribute("class", "apple collidable");
+        entity.setAttribute("random-position", "fixedY: 1.25");
+        this.el.sceneEl.appendChild(entity);
+      }
+    }
   }
 });
