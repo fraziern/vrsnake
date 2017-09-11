ff726fd4c1b1be6d528f0480e68716f90c1fa25c
Nick Frazier <nrflaw@gmail.com>
2017-09-10 21:23:13 -0400

    minor tweaks

--- components/wasd-relative.js
+++ components/wasd-relative.js
@@ -9,12 +9,15 @@ AFRAME.registerComponent("wasd-relative", {
 
   init: function() {
     this.newVelocity = new THREE.Vector3(this.data.speed, 0, 0); // reusable vector
-    this.onKeyDown = this.onKeyDown.bind(this);
-    window.addEventListener("keydown", this.onKeyDown, false);
+    this.down = false; // used to prevent repetitive keydowns
+    window.addEventListener("keydown", this.onKeyDown.bind(this), false);
+    window.addEventListener("keyup", this.onKeyUp.bind(this), false);
     this.el.sceneEl.emit("changemomentum", this.newVelocity);
   },
 
   onKeyDown: function(e) {
+    if (this.down) return;
+    this.down = true;
     const rotate = function(vec, rad) {
       // rotate about x-z plane
       const x = vec.x * Math.cos(rad) - vec.z * Math.sin(rad);
@@ -24,9 +27,11 @@ AFRAME.registerComponent("wasd-relative", {
 
     let rotated = {};
     switch (e.keyCode) {
+    case 37:
     case 65: // left
       rotated = rotate(this.newVelocity, -Math.PI / 2);
       break;
+    case 39:
     case 68: // right
       rotated = rotate(this.newVelocity, Math.PI / 2);
       break;
@@ -35,5 +40,9 @@ AFRAME.registerComponent("wasd-relative", {
     }
     this.newVelocity.set(rotated.x, rotated.y, rotated.z);
     this.el.sceneEl.emit("changemomentum", this.newVelocity);
+  },
+
+  onKeyUp: function() {
+    this.down = false;
   }
 });
